0c0c95b3ddeba3abb7bc4bc5b97c2c5d91ae754d
Hans Meyer <Hans.Meyer@team-beverage.de>
2024-04-24 16:38:50 +0200

    wirklich nur laufende Spiele

--- src/Kickern/GraphQL/QueryType.cs
+++ src/Kickern/GraphQL/QueryType.cs
@@ -4,6 +4,6 @@ namespace Kickern.GraphQL
 {
     public class QueryType
     {
-        public async Task<IQueryable<KickerSpiel>> LaufendeSpiele([Service] IKickerspielRepository kickerspielRepository) => await kickerspielRepository.GetSpiele();
+        public async Task<IQueryable<KickerSpiel>> LaufendeSpiele([Service] IKickerspielRepository kickerspielRepository) => (await kickerspielRepository.GetSpiele()).Where(spiel => !spiel.endzeit.HasValue);
     }
 }
